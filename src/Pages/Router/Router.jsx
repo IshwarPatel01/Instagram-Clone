5a55892339fb3317ba359ee83cf9df84f48099d3
Ishwar Patel <ishwarrpatel50@gmail.com>
2024-11-20 22:55:50 +0530

    add react app

---
+++ src/Pages/Router/Router.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import Homepage from "../Homepage/Homepage";
+const Router = () => {
+    return (
+        <div>
+            <div className="flex ">
+                <div>
+                    <Sidebar />
+                </div>
+                <div>
+                    <Routes>
+                        <Route path="/" element={<Homepage />}></Route>
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Router
